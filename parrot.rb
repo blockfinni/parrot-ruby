a6272c400d46b17a331f123a7d78b77fc0fbb76c
Benjamin_Ajisefinni <benfinnirr@gmail.com>
2021-01-19 15:39:29 -0900

    commit tree 022b6052b528b2c23dacc21d8004fee68f9aca36

--- parrot.rb
+++ parrot.rb
@@ -4,7 +4,7 @@
 
 # This function contains a default argument, with the option to accept other arguments
 
-def parrot(parrot = "Squawk!")
-  puts parrot
- parrot 
+def parrot(pharse = "Squawk!")
+  puts pharse
+ pharse 
 end
